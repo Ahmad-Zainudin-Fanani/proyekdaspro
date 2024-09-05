70e7f249c8778955bc98c02aa7e7aae16e30016d
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-09-05 21:30:43 +0700

    Ahmad Zainudin

---
+++ AhmadZainudin.java
@@ -0,0 +1,13 @@
+public class AhmadZainudin {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Ahmad Zainudin Fanani");
+        System.out.println("NIM     : 244107020051");
+        System.out.println("Kelas   : TI 1H");
+        System.out.println("No Absen: 02");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : JL. Bunga Desember 26");
+        System.out.println("No HP   : 0858-1505-2369");
+        System.out.println("Hobi    : Menonton Film");
+    
+    }
+}
