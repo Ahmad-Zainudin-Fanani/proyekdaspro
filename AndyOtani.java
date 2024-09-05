62705c1881e29578607a84d37dcf00f678fbc623
Ahmad-Zainudin-Fanani <azfann.natinons@gmail.com>
2024-09-05 21:35:36 +0700

    AndyOtani

---
+++ AndyOtani.java
@@ -0,0 +1,13 @@
+public class AndyOtani {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Andy Otani Dipo Yudho");
+        System.out.println("NIM     : 244107020241");
+        System.out.println("Kelas   : TI 1H");
+        System.out.println("No Absen: 04");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Perum. Landungsari Indah Blok K25");
+        System.out.println("No HP   : 089524637834");
+        System.out.println("Hobi    : Renang");
+    
+    }
+}
